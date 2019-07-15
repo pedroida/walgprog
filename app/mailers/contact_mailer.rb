fe1d3e49a29e4b3b05b0691ad8770d35937670f9
pedroida <pedrohsida@hotmail.com>
2019-07-15 20:51:25 -0300

    using emails templates in mailers

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,31 @@
 class ContactMailer < ApplicationMailer
+  include ActionView::Helpers::UrlHelper
+
   def welcome
     @contact = params[:contact]
+    @template = EmailTemplate.find 1
+    @update_link = link_to @template.update_link_title, contact_edit_url(@contact,
+                                                                         @contact.update_token)
+    @unregister_link = link_to @template.unregister_link_title,
+                               contact_unregister_confirmation_url(@contact,
+                                                                   @contact.unregister_token)
+
+    @content = @template.get_replaced_content(@update_link, @unregister_link, @contact.name)
+
     mail(to: @contact.email_with_name, subject: I18n.t('mail.welcome_email.subject'))
   end
 
   def success_update
     @contact = params[:contact]
+    @template = EmailTemplate.find 2
+    @update_link = link_to @template.update_link_title, contact_edit_url(@contact,
+                                                                         @contact.update_token)
+    @unregister_link = link_to @template.unregister_link_title,
+                               contact_unregister_confirmation_url(@contact,
+                                                                   @contact.unregister_token)
+
+    @content = @template.get_replaced_content(@update_link, @unregister_link, @contact.name)
+
     mail(to: @contact.email_with_name, subject: I18n.t('mail.updated.subject'))
   end
 end
